7b4ba4a201a90a8d55bff5c00304d375ddda8198
Arjunan-Eas <arjunan.eas@gmail.com>
2024-08-30 18:59:35 -0700

    other stuff

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=c:\Users\arjun\Documents\ARKbot\.venv
+set VIRTUAL_ENV=c:\Users\arjun\OneDrive\Documents\ARKbot\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
